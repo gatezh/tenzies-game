7fdff4cfc5db60f37c27a5bfb099b14de739b6bc
Sergii Gatezh <2880401+gatezh@users.noreply.github.com>
2025-02-13 10:14:33 -0700

    Add instruction and styling

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,13 @@ export default function App() {
 
   return (
     <main>
+      <header>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </header>
       <div className="board">
         {board.map(({ id, value, isHeld }) => (
           <Die
